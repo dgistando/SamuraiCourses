5dbebd09b180c66b277def04742b2669286d3e77
dgistando <davidgistando@gmail.com>
2016-04-05 20:49:27 -0700

    some error with server. fixing...

--- SamuariCourses/app/src/main/java/com/ninja/cse/samuaricourses/NinjaActivity.java
+++ SamuariCourses/app/src/main/java/com/ninja/cse/samuaricourses/NinjaActivity.java
@@ -1,10 +1,12 @@
 package com.ninja.cse.samuaricourses;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
@@ -35,13 +38,25 @@ import com.microsoft.windowsazure.mobileservices.table.sync.localstore.SQLiteLoc
 import com.microsoft.windowsazure.mobileservices.table.sync.synchandler.SimpleSyncHandler;
 import com.squareup.okhttp.OkHttpClient;
 
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.*;
 
 
 
 public class NinjaActivity extends AppCompatActivity {
+
+    private MobileServiceClient mClient;
+    private MobileServiceTable<courses> mCoursesTable;
+    private ProgressBar mProgressBar;
+
 
     private AutoCompleteTextView department,classes;
     private ArrayAdapter<String> departmentAdapter,classesAdapter;
@@ -54,26 +69,50 @@ public class NinjaActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        classesAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,classeslist);
+        classesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, classeslist);
         classes = (AutoCompleteTextView) findViewById(R.id.number);
         classes.setThreshold(1);
         classes.setAdapter(classesAdapter);
         classes.setHint("Class");
 
-        departmentAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+        departmentAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.Department_array));
         department = (AutoCompleteTextView) findViewById(R.id.department);
         department.setAdapter(departmentAdapter);
         department.setThreshold(1);
         department.setHint("Department");
 
+        Log.d("Created","\nThis is the message\n");
 
-        department.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-            }
-        });
+        try {
+            mClient = new MobileServiceClient(
+                    "https://samuraicourses.azurewebsites.net",
+                    this).withFilter(new ProgressFilter());
+
+            mClient.setAndroidHttpClientFactory(new OkHttpClientFactory() {
+                @Override
+                public OkHttpClient createOkHttpClient() {
+                    OkHttpClient client = new OkHttpClient();
+                    client.setReadTimeout(20, TimeUnit.SECONDS);
+                    return client;
+                }
+            });
+
+            mCoursesTable = mClient.getTable(courses.class);
+
+            department.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    classeslist = getClasseslist(parent.getItemAtPosition(position).toString());
+                }
+            });
+
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
@@ -99,5 +138,85 @@ public class NinjaActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private List<String> getClasseslist(final String selectedDepartment){
+
+        final List<String> classesList = new ArrayList<>();
+
+        AsyncTask<Void,Void,Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try{
+                    final List<courses> classes = availableClasses(selectedDepartment);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            for(courses number : classes){
+                                classesList.add(number.getNumber());
+                            }
+
+                            Set<String> hashedset = new HashSet<>();
+                            hashedset.addAll(classesList);
+                            classesList.clear();
+                            classesList.addAll(hashedset);
+                        }
+                    });
+
+                }catch(final Exception e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+        return classesList;
+    }
+
+    private List<courses> availableClasses(String item) throws ExecutionException, InterruptedException{
+            List<courses> entities =  mCoursesTable.where().startsWith("number",item).execute().get();
+        return entities;
+    }
+
+    private class ProgressFilter implements ServiceFilter {
+
+        @Override
+        public ListenableFuture<ServiceFilterResponse> handleRequest(ServiceFilterRequest request, NextServiceFilterCallback nextServiceFilterCallback) {
+
+            final SettableFuture<ServiceFilterResponse> resultFuture = SettableFuture.create();
+
+
+            runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+            });
+
+            ListenableFuture<ServiceFilterResponse> future = nextServiceFilterCallback.onNext(request);
+
+            Futures.addCallback(future, new FutureCallback<ServiceFilterResponse>() {
+                @Override
+                public void onFailure(Throwable e) {
+                    resultFuture.setException(e);
+                }
+
+                @Override
+                public void onSuccess(ServiceFilterResponse response) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.GONE);
+                        }
+                    });
+
+                    resultFuture.set(response);
+                }
+            });
+
+            return resultFuture;
+        }
     }
 }
