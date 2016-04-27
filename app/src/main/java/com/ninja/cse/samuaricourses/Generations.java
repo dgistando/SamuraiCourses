e6c773db4c2d861c5a5a2b9cda0fda0e1a3fdcd6
econtreras7 <econtreras7@ucmerced.edu>
2016-04-26 19:57:58 -0700

    Can generate schedules

--- app/src/main/java/com/ninja/cse/samuaricourses/Generations.java
+++ app/src/main/java/com/ninja/cse/samuaricourses/Generations.java
@@ -1,7 +1,5 @@
 package com.ninja.cse.samuaricourses;
 
-import android.util.Log;
-
 import com.alamkanak.weekview.WeekViewEvent;
 
 import java.util.ArrayList;
@@ -14,85 +12,60 @@ import java.util.List;
  * Website: http://alamkanak.github.io
  */
 public class Generations extends BaseGenerations {
-
-    int j=0;
+    static boolean check=false;
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
 
-        ArrayList<courses> listOfCourses = new ArrayList<courses>();
-        int j = this.getIntent().getIntExtra("ScheduleSize", 0);
 
-        for(int i=0;i<j;i++){
+
+        ArrayList<courses> listOfCourses = new ArrayList<courses>();
+        int numOfSchedules = this.getIntent().getIntExtra("ScheduleSize", 0);
+        List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+        Calendar startTime = Calendar.getInstance();
+        Calendar endTime = (Calendar) startTime.clone();
+        for (int i = 0; i < numOfSchedules; i++) {
             listOfCourses.clear();
             listOfCourses.addAll((ArrayList) this.getIntent().getParcelableArrayListExtra("schedule: " + i));
-            for(int k=0;k<listOfCourses.size();k++){
-                Log.d("RESULTS: ", listOfCourses.get(k).getNumber() + "");
+            for (int k = 0; k < listOfCourses.size(); k++) {
+                for (int d = 0; d < listOfCourses.get(k).getDays().length(); d++) {
+                    startTime.set(Calendar.HOUR_OF_DAY, listOfCourses.get(k).getStartTime() / 100);
+                    startTime.set(Calendar.DAY_OF_MONTH, 22);
+                    startTime.set(Calendar.MINUTE, listOfCourses.get(k).getStartTime() % 100);
+                    startTime.set(Calendar.MONTH, 6);//set month one month before the set month of august
+                    startTime.add(Calendar.MONTH, 1);//adding one month to July, makes August
+                    switch (listOfCourses.get(k).getDays().charAt(d)) {
+                        case 'M':
+                            startTime.add(Calendar.DAY_OF_MONTH, 0);
+                            break;
+                        case 'T':
+                            startTime.add(Calendar.DAY_OF_MONTH, 1);
+                            break;
+                        case 'W':
+                            startTime.add(Calendar.DAY_OF_MONTH, 2);
+                            break;
+                        case 'R':
+                            startTime.add(Calendar.DAY_OF_MONTH, 3);
+                            break;
+                        case 'F':
+                            startTime.add(Calendar.DAY_OF_MONTH, 4);
+                            break;
+                    }
+                    startTime.add(Calendar.WEEK_OF_YEAR, i);//0-number of schedules,0:first week
+                    startTime.set(Calendar.YEAR, newYear);
+                    endTime = (Calendar) startTime.clone();
+                    endTime.set(Calendar.HOUR_OF_DAY, listOfCourses.get(k).getEndTime() / 100);
+                    endTime.set(Calendar.MINUTE, listOfCourses.get(k).getEndTime() % 100);
+                    WeekViewEvent event = new WeekViewEvent(0, listOfCourses.get(k).getNumber(),
+                            startTime, endTime);
+                    event.setColor(getResources().getColor(R.color.event_color_01));
+                    events.add(event);
+                }
+                startTime = Calendar.getInstance();
             }
-            Log.d("RESULTS: ", "=========================");
         }
-
-        // Populate the week view with some events.
-        List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
-
-        Calendar startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 3);
-        startTime.set(Calendar.DAY_OF_MONTH, 22);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, 6);//set month one month before the set month of august
-        startTime.add(Calendar.MONTH,1);//adding one month to July, makes August
-        startTime.add(Calendar.WEEK_OF_YEAR,5);//now you can increment by weeks, number means, + or - week
-        startTime.set(Calendar.YEAR, newYear);
-        Calendar endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR, 1);
-        WeekViewEvent event = new WeekViewEvent(0,
-                "ANTH-005-02L\n" +
-                "Biological Anthropology\n" +
-                "8:00-8:50am\n" +
-                "SSM 107\n" +
-                "Staff",
-                startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_01));
-        events.add(event);
-
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 3);
-        startTime.set(Calendar.DAY_OF_MONTH, 30);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, 7);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR, 4);
-        event = new WeekViewEvent(0, "Class02", startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_01));
-        events.add(event);
-
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 3);
-        startTime.set(Calendar.MINUTE, 30);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.set(Calendar.HOUR_OF_DAY, 4);
-        endTime.set(Calendar.MINUTE, 30);
-        endTime.set(Calendar.MONTH, newMonth-1);
-        event = new WeekViewEvent(1, "another blah", startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_02));
-        events.add(event);
-
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 4);
-        startTime.set(Calendar.MINUTE, 20);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime = (Calendar) startTime.clone();
-        endTime.set(Calendar.HOUR_OF_DAY, 5);
-        endTime.set(Calendar.MINUTE, 0);
-        event = new WeekViewEvent(2, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_03));
-        events.add(event);
-
         return events;
     }
+
+
 
 }
