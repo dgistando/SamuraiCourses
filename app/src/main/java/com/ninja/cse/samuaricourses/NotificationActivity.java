6655b9807abd7c5c51748b010e13bad0b292896e
dgistando <davidgistando@gmail.com>
2016-04-26 19:04:34 -0700

    added notification activity

---
+++ app/src/main/java/com/ninja/cse/samuaricourses/NotificationActivity.java
@@ -0,0 +1,15 @@
+package com.ninja.cse.samuaricourses;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dgist on 4/26/2016.
+ */
+public class NotificationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
