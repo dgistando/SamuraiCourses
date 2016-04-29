bd1ccbd8793b322e621034d25de7d0659beee43d
dgistando <davidgistando@gmail.com>
2016-04-29 08:51:50 -0700

    started nofication activity

--- app/src/main/java/com/ninja/cse/samuaricourses/NotificationActivity.java
+++ app/src/main/java/com/ninja/cse/samuaricourses/NotificationActivity.java
@@ -2,9 +2,16 @@ package com.ninja.cse.samuaricourses;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+
+import java.util.ArrayList;
 
 /**
  * Created by dgist on 4/26/2016.
@@ -12,13 +19,52 @@ import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
 
 public class NotificationActivity extends AppCompatActivity {
 
-    private MobileServiceClient mClient;
-    private MobileServiceTable<courses> mCoursesTable;
+    AutoCompleteTextView Department, Classes;
+    private ArrayAdapter<String> departmentAdapter, classesAdapter;
+    ArrayList<String> classeslist = new ArrayList<>();
+    Button add,notify;
+
+
+    DBHelper db;
+    //removed from layout: <android:layout_alignEnd="@+id/btnTrack" under add button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification);
+        add = (Button)findViewById(R.id.btnAdd);
+        notify = (Button)findViewById(R.id.notify);
+
+        classesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, classeslist);
+        Classes = (AutoCompleteTextView) findViewById(R.id.number);
+        Classes.setThreshold(1);
+        Classes.setAdapter(classesAdapter);
+        Classes.setHint("Class");
+
+        departmentAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.Department_array));
+        Department = (AutoCompleteTextView) findViewById(R.id.department);
+        Department.setAdapter(departmentAdapter);
+        Department.setThreshold(1);
+        Department.setHint("Department");
+
+
+
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Department.getText().equals("") || Classes.getText().equals("")){
+                    Toast.makeText(NotificationActivity.this,"fill all fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/ninja/cse/samuaricourses/NotificationService.java
+++ app/src/main/java/com/ninja/cse/samuaricourses/NotificationService.java
@@ -1,9 +1,43 @@
 package com.ninja.cse.samuaricourses;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**
  * Created by dgist on 4/29/2016.
  */
 public class NotificationService extends Service {
+
+    public static boolean serviceStarted = false;
+
+    public NotificationService() {
+        super();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        serviceStarted = true;
+        Toast.makeText(NotificationService.this, "Service was started", Toast.LENGTH_SHORT).show();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        serviceStarted = false;
+        Toast.makeText(NotificationService.this, "Service was stopped", Toast.LENGTH_SHORT).show();
+    }
 }
